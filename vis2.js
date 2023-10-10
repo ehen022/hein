35a3c1dafce86e6991bbfb4db85d0b752824f07d
ehen022 <147455876+ehen022@users.noreply.github.com>
2023-10-10 09:29:14 +0700

    Update vis2.js

--- vis2.js
+++ vis2.js
@@ -1,7 +1,13 @@
 var vg_1 = "vis2.json";
-vegaEmbed("#bubble_chart", vg_1)
-  .then(function (result) {
-    // Access the Vega view instance
-    //https://vega.github.io/vega/docs/api/view/) as result.view
-  })
-  .catch(console.error);
+
+vegaEmbed("#bubble_chart", vg_1).then(function(result) {
+// Access the Vega view instance
+ //https://vega.github.io/vega/docs/api/view/) as result.view
+
+    }) .catch(console.error);
+    var vg_2="chloropeth.json";
+vegaEmbed("#world_map", vg_2).then(function(result) {
+      // Access the Vega view instance
+       //https://vega.github.io/vega/docs/api/view/) as result.view
+      
+          }) .catch(console.error);
